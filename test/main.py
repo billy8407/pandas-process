814e274382fc1d07494907a34d656626998de93a
Bill <billy8407@gmail.com>
2021-03-12 02:48:07 +0800

    fix testing delete bug

--- test/main.py
+++ test/main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+
 import os
+import sys
+
 file_name = input('請輸入 excel 名稱(xlsx 檔): ')
 print('程式啟動中...')
 #xlsx_name = "../finance_data/" + file_name + ".csv"
@@ -23,25 +26,36 @@ for col in df_columns:
 
 
 row_num = df.shape[0]
-delte_row_index=[]
+delete_row_index=[]
 
 new_date = True
+new_maturity_month = ''
 
-for row in range(500):
+for row in range(2000):
     maturity_month = df.loc[row, 'maturity_month'].replace(' ','')
     if len(maturity_month) != 6:
-        delte_row_index.append(row)
+        delete_row_index.append(row)
     
-    elif new_date == True:
+    if new_date == True:
         now_date = df.loc[row, 'date']
+        new_maturity_month = maturity_month 
         new_date = False
     
-    elif df.loc[row, 'date'] == now_date:
-        pass
-    
     else:
-        delte_row_index.append(row)
+        if now_date == df.loc[row, 'date']:
+            if new_maturity_month != df.loc[row, 'maturity_month'].replace(' ',''):
+                delete_row_index.append(row)
+            else:
+                pass
+        else: #重置excel
+            now_date = df.loc[row, 'date']
+            new_maturity_month = maturity_month 
+            
 
-df = df.drop(delte_row_index, axis=0)
 
-print(df)
+    
+
+df = df.drop(delete_row_index, axis=0)
+
+df.to_csv('%s_output.csv' %file_name, encoding=sys.getfilesystemencoding(),index=False)
+#print(df)
